bae2c6bbfee281058f0ad6b05527722f2d1b9076
monika-oss <monikababu18@gmail.com>
2026-07-13 08:06:02 +0530

    Allow all hosts in ALLOWED_HOSTS for production deployment

--- backend/smart_campus/settings.py
+++ backend/smart_campus/settings.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-secret-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
